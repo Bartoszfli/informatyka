25b3b9687b94400be7e61c757a2ecd0d176c94da
Bartosz <bufon523@outlook.com>
2023-10-25 14:08:10 +0200

    Komentarz

---
+++ lekcja 1 struktura programu/1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	short liczba = 2;
+	float zmiennoprzecinkowa = 3.532;
+	string tekst = "tekst";
+	char znak = 'A';
+	bool znakowy = false;
+	
+	cout << "Hello world" << endl;
+	cout << "Liczba Ca³kowita: " << liczba;
+	cout << endl;
+	
+	string jakos;
+	cout << "Podaj jakis wyraz: ";
+	cin >> jakos;
+	cout << "Wprowadziles wartosc: " << jakos;
+	
+	return 0;
+}
