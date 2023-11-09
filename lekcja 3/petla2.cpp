6ac57aaf5297dd2923e37ca61fca9b3d92bb15ac
Bartosz <bufon523@outlook.com>
2023-11-09 15:01:57 +0100

    Komentarz

---
+++ lekcja 3/petla2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	 for(int j = 0; j < 11; j++){	 
+	for(int i = 0; i < 11; i++){
+		
+    cout <<  i  << " * " << j <<  " = "  << i*j << endl;
+}
+}
+	 cout << endl;
+    system("pause");	
+}
